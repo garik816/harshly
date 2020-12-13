2a2a0ed2d6c2fac5633fe44c73673f91b82808cc
garik816 <garikpost@rambler.ru>
2020-12-13 16:59:19 +0200

    first blood

---
+++ sheet.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+import random
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
+random.seed()
+
+CREDENTIALS_FILE = 'creds.json'
+spreadsheet_id = '1HsSubLXetftZ8GCocLplpi_aMJGdjR7cFje3BimiA_o'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
+
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets','v4', http = httpAuth)
+
+
+values = service.spreadsheets().values().get(
+    spreadsheetId = spreadsheet_id,
+    range = 'D3:D',
+    majorDimension = 'ROWS'
+).execute()
+pprint (values)
+
+
+exit()
+
