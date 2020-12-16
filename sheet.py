e3fe2f0bb1e46c9dbb208bbcb62d8e067cc9fe8f
garik816 <garikpost@rambler.ru>
2020-12-16 20:01:37 +0200

    Update sheet.py

--- sheet.py
+++ sheet.py
@@ -1,27 +1,54 @@
 from pprint import pprint
 
-import random
 import httplib2
+import json
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 
-random.seed()
+import telebot
+
+token = 'токен телеграмм бота'
+bot = telebot.TeleBot(token)
+
 
 CREDENTIALS_FILE = 'creds.json'
-spreadsheet_id = 'spreadsheet_id'
+spreadsheet_id = 'id таблици'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
-
 httpAuth = credentials.authorize(httplib2.Http())
 service = apiclient.discovery.build('sheets','v4', http = httpAuth)
 
 
-values = service.spreadsheets().values().get(
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    transmitter()
+    bot.send_message(message.chat.id, receiver())
+
+def transmitter():
+    transmit = service.spreadsheets().values().batchUpdate(
     spreadsheetId = spreadsheet_id,
-    range = 'D3:D',
-    majorDimension = 'ROWS'
-).execute()
-pprint (values)
+    body = {
+        "valueInputOption": "USER_ENTERED",
+        "data": [
+            {"range": "J1:J2",
+             "majorDimension": 'ROWS',
+             "values": [["=TODAY()"]]}
+        ]
+    }
+    ).execute()
 
+def receiver():
+    receiver = service.spreadsheets().values().get(
+        spreadsheetId = spreadsheet_id,
+        range = 'I5:I9',
+        majorDimension = 'ROWS'
+    ).execute()
 
-exit()
+    txt = json.dumps(receiver, ensure_ascii=False)
+    txt = txt.replace("[\"", "")
+    txt = txt.replace("\"]","")
+    txt = txt.replace("]}","")
+    txt = txt.replace("{\"range\": \"'Лист1'!I5:I9\", \"majorDimension\": \"ROWS\", \"values\": [","")
+    return txt
 
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
